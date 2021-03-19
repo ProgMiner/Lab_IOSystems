fa3bc30d04d00e5e4f97ab2aaa960facb6a49564
avvessalom <avvessalom@gmail.com>
2021-03-19 12:48:43 +0300

    add bench

---
+++ lab2/benchmark.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+  sudo "$0" "$@"
+  exit $?
+fi
+
+mkfs.vfat /dev/mydisk1
+mkfs.vfat /dev/mydisk5
+mkfs.vfat /dev/mydisk6
+
+mkdir /mnt/disk1
+mkdir /mnt/disk5
+mkdir /mnt/disk6
+
+mount /dev/mydisk1 /mnt/disk1
+mount /dev/mydisk5 /mnt/disk5
+mount /dev/mydisk6 /mnt/disk6
+
+function create_files() {
+  dd if=/dev/urandom of=/mnt/disk1/file bs=1M count=5
+  dd if=/dev/urandom of=/mnt/disk5/file bs=1M count=5
+  dd if=/dev/urandom of=/mnt/disk6/file bs=1M count=5
+}
+
+function delete_files() {
+  rm /mnt/disk1/file
+  rm /mnt/disk5/file
+  rm /mnt/disk6/file
+}
+
+create_files
+
+  echo "Copying files within virtual disk"
+  pv /mnt/disk1/file > /mnt/disk6/file
+  pv /mnt/disk5/file > /mnt/disk6/file
+  pv /mnt/disk6/file > /mnt/disk1/file
+
+delete_files
+
+create_files
+  echo "Copying files from virtual file to real disk"
+  mkdir /tmp/io
+  pv /mnt/disk1/file > /tmp/io/testfile
+  pv /mnt/disk5/file > /tmp/io/testfile
+  pv /mnt/disk6/file > /tmp/io/testfile
+delete_files
